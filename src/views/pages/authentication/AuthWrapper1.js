840ae739f3ab24b8d5d3b69e6bcf14be4360d06f
Leonardo Horiba <leonardohoriba@gmail.com>
2022-03-22 11:57:44 -0300

    login and register pages

--- src/views/pages/authentication/AuthWrapper1.js
+++ src/views/pages/authentication/AuthWrapper1.js
@@ -1,11 +1,13 @@
 // material-ui
 import { styled } from '@mui/material/styles';
-
+import BackgroundImage from '../../../assets/images/login/dqbrn_background.jpg';
 // ==============================|| AUTHENTICATION 1 WRAPPER ||============================== //
 
 const AuthWrapper1 = styled('div')(({ theme }) => ({
     backgroundColor: theme.palette.primary.light,
-    minHeight: '100vh'
+    minHeight: '100vh',
+    backgroundImage: `url(${BackgroundImage})`,
+    backgroundSize: 'cover'
 }));
 
 export default AuthWrapper1;
