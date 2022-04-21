0c27fe8acb3ef55ca1229d5ca3035930b4d415d7
pedrobpn <pbpn2000@hotmail.com>
2022-04-21 12:38:40 -0300

    crud buttons created

--- src/views/admin/index.js
+++ src/views/admin/index.js
@@ -6,9 +6,12 @@ import Autocomplete from '@mui/material/Autocomplete';
 import MainCard from 'ui-component/cards/MainCard';
 import Button from '@mui/material/Button';
 import { useState } from 'react';
+
+// other imports
 import { deseases } from '../map/deseases/desease1';
 import brStates from '../map/brStates';
 import SearchTable from './table';
+import ModalCreate from './ModalCreate';
 
 // ==============================|| SAMPLE PAGE ||============================== //
 
@@ -19,52 +22,58 @@ function Admin() {
     const [stateOption] = useState(brStates[0]);
     return (
         <MainCard title="DADOS SANITÁRIOS">
-            <div style={{ display: 'flex', paddingBottom: 10 }}>
+            <div style={{ display: 'flex', paddingBottom: 10, justifyContent: 'space-between' }}>
+                <ModalCreate value="Criar" />
+                <ModalCreate value="Atualizar" />
+                <ModalCreate value="Deletar" />
+                <ModalCreate value="Upload CSV" />
                 <div style={{ display: 'flex' }}>
-                    <Autocomplete
-                        id="desease_select"
-                        options={deseases}
-                        autoComplete
-                        includeInputInList
-                        renderInput={(params) => <TextField {...params} label="Doença" />}
-                        sx={{ width: 150 }}
-                        value={deseases.find((option) => option.currentTarget?.getAttribute('data-option-index') === deseaseOption)}
-                        /* onChange={async (option) => {
-                            console.log('deseases');
-                            const op = option.currentTarget.getAttribute('data-option-index');
-                            console.log('option: ', op);
+                    <div style={{ display: 'flex' }}>
+                        <Autocomplete
+                            id="desease_select"
+                            options={deseases}
+                            autoComplete
+                            includeInputInList
+                            renderInput={(params) => <TextField {...params} label="Doença" />}
+                            sx={{ width: 150 }}
+                            value={deseases.find((option) => option.currentTarget?.getAttribute('data-option-index') === deseaseOption)}
+                            /* onChange={async (option) => {
+                                console.log('deseases');
+                                const op = option.currentTarget.getAttribute('data-option-index');
+                                console.log('option: ', op);
 
-                            if (op !== deseaseOption) {
-                                setDesease(op);
-                                console.log('deseaseOption: ', deseaseOption);
-                            }
-                        }} */
-                    />
-                </div>
-                <div style={{ display: 'flex' }}>
-                    <Autocomplete
-                        id="state_select"
-                        options={brStates}
-                        autoComplete
-                        includeInputInList
-                        renderInput={(params) => <TextField {...params} label="Estado" />}
-                        sx={{ width: 150 }}
-                        value={deseases.find((option) => option.currentTarget?.getAttribute('data-option-index') === stateOption)}
-                        /* onChange={async (option) => {
-                            console.log('states');
-                            const op = option.currentTarget.getAttribute('data-option-index');
-                            console.log('option: ', op);
+                                if (op !== deseaseOption) {
+                                    setDesease(op);
+                                    console.log('deseaseOption: ', deseaseOption);
+                                }
+                            }} */
+                        />
+                    </div>
+                    <div style={{ display: 'flex' }}>
+                        <Autocomplete
+                            id="state_select"
+                            options={brStates}
+                            autoComplete
+                            includeInputInList
+                            renderInput={(params) => <TextField {...params} label="Estado" />}
+                            sx={{ width: 150 }}
+                            value={deseases.find((option) => option.currentTarget?.getAttribute('data-option-index') === stateOption)}
+                            /* onChange={async (option) => {
+                                console.log('states');
+                                const op = option.currentTarget.getAttribute('data-option-index');
+                                console.log('option: ', op);
 
-                            if (op !== stateOption) {
-                                setState(op);
-                                console.log('stateOption: ', stateOption);
-                            }
-                        }} */
-                    />
+                                if (op !== stateOption) {
+                                    setState(op);
+                                    console.log('stateOption: ', stateOption);
+                                }
+                            }} */
+                        />
+                    </div>
+                    <Button variant="contained" color="primary">
+                        Buscar
+                    </Button>
                 </div>
-                <Button variant="contained" color="primary">
-                    Buscar
-                </Button>
             </div>
             <div style={{ display: 'flex' }}>
                 <SearchTable />
