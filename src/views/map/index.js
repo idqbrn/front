499168210974b2f33a7142444f903051d707a81a
pedrobpn <pbpn2000@hotmail.com>
2022-05-30 10:15:37 -0300

    merge errors fixed

--- src/views/map/index.js
+++ src/views/map/index.js
@@ -9,8 +9,8 @@ import React, { useState, useEffect } from 'react';
 
 import brStates from './brStates';
 import { diseases, disease1 } from './diseases/disease1';
-import JsonLatLng from './LocalLatLng/states_latitudes_flat_name.json';
-import { /* vecNumCityState, */ vecPosCityState, CitiesFromState } from './LocalLatLng/vecCityState';
+/* import JsonLatLng from './LocalLatLng/states_latitudes_flat_name.json';
+import { vecNumCityState,  vecPosCityState, CitiesFromState } from './LocalLatLng/vecCityState'; */
 
 import brazilBorders from './LocalLatLng/brazil_borders.json';
 
@@ -147,7 +147,6 @@ function Map() {
         });
     });
 
-
     const onUnmount = React.useCallback(() => {
         setMap(null);
     }, []);
@@ -165,51 +164,15 @@ function Map() {
                         <input type="hidden" id="localLng" name="localLng" />
                         <input type="hidden" id="viewport" name="viewport" />
                     </div>
-                    {/* <div style={{ display: 'flex', padding: 8 }}>
-                        <Autocomplete
-                            id="center_select"
-                            options={brStates}
-                            autoComplete
-                            includeInputInList
-                            renderInput={(params) => <TextField {...params} label="Local" />}
-                            sx={{ width: 300 }}
-                            value={brStates.find(
-                                (option) => brStates[option.nativeEvent?.path[0].getAttribute('data-option-index')]?.center === centerOption
-                            )}
-                            onChange={async (option) => {
-                                const op = parseInt(option.nativeEvent.path[0].getAttribute('data-option-index'), 10);
-
-                                setState(op);
-                                if (op >= 0) {
-                                    const local = brStates[op];
-
-                                    map.setZoom(brStates[0].zoom);
-
-                                    await sleep(1000);
-                                    setCenter(local.center);
-                                    map.panTo(local.center);
-
-                                    if (local.zoom > brStates[0].zoom) map.setZoom(local.zoom);
-
-                                    const cities = [];
-
-                                    // eslint-disable-next-line no-plusplus
-                                    for (let i = vecPosCityState[op]; i < vecPosCityState[op + 1]; i++) cities.push(JsonLatLng[i].nome);
-
-                                    setCities(cities);
-                                }
-                            }}
-                        />
-                    </div> */}
                     <div style={{ display: 'flex', padding: 8 }}>
                         <Autocomplete
-                            id="desease_select"
-                            options={deseases}
+                            id="disease_select"
+                            options={diseases}
                             autoComplete
                             includeInputInList
                             renderInput={(params) => <TextField {...params} label="Doença" />}
                             sx={{ width: 300 }}
-                            value={deseases.find((option) => option?.currentTarget?.getAttribute('data-option-index') === deseaseOption)}
+                            value={diseases.find((option) => option?.currentTarget?.getAttribute('data-option-index') === diseaseOption)}
                             onChange={async (option) => {
                                 const op = parseInt(option?.currentTarget?.getAttribute('data-option-index'), 10);
                                 setDesease(op);
@@ -222,7 +185,7 @@ function Map() {
 
                                 if (!Number.isNaN(op)) {
                                     for (let i = 0; i < 100; i += 1) {
-                                        fakeData?.push(new window.google.maps.LatLng(desease1[op][i].lat, desease1[op][i].lng));
+                                        fakeData?.push(new window.google.maps.LatLng(disease1[op][i].lat, disease1[op][i].lng));
                                     }
                                 }
 
@@ -240,109 +203,7 @@ function Map() {
                         zoom={zoomMap}
                         onLoad={onLoad}
                         onUnmount={onUnmount}
-
-                    >
-                        <div style={{ flexDirection: 'column', display: 'flex' }}>
-                            <div style={{ display: 'flex', padding: 8, paddingTop: 12 }}>
-                                <Autocomplete
-                                    id="center_select"
-                                    options={brStates}
-                                    autoComplete
-                                    includeInputInList
-                                    renderInput={(params) => <TextField {...params} label="Local" />}
-                                    sx={{ width: 300 }}
-                                    value={brStates.find(
-                                        (option) =>
-                                            brStates[option.nativeEvent?.path[0].getAttribute('data-option-index')]?.center === centerOption
-                                    )}
-                                    onChange={async (option) => {
-                                        const op = parseInt(option.nativeEvent.path[0].getAttribute('data-option-index'), 10);
-
-                                        setState(op);
-                                        if (op >= 0) {
-                                            const local = brStates[op];
-
-                                            map.setZoom(brStates[0].zoom);
-
-                                            await sleep(1000);
-                                            setCenter(local.center);
-                                            map.panTo(local.center);
-
-                                            if (local.zoom > brStates[0].zoom) map.setZoom(local.zoom);
-
-                                            const cities = CitiesFromState(op);
-
-                                            setCities(cities);
-                                        }
-                                    }}
-                                />
-                            </div>
-                            <div style={{ display: 'flex', padding: 8 }}>
-                                <Autocomplete
-                                    id="city_select"
-                                    options={citiesState}
-                                    autoComplete
-                                    includeInputInList
-                                    renderInput={(params) => <TextField {...params} label="Cidade" />}
-                                    sx={{ width: 300 }}
-                                    value={brStates.find(
-                                        (option) =>
-                                            brStates[option.nativeEvent?.path[0].getAttribute('data-option-index')]?.center === cityOption
-                                    )}
-                                    onChange={async (option) => {
-                                        const op = parseInt(option.nativeEvent.path[0].getAttribute('data-option-index'), 10);
-                                        if (op >= 0) {
-                                            const cityNum = op + vecPosCityState[stateOption];
-
-                                            setCity(cityNum);
-
-                                            const local = JsonLatLng[cityNum];
-
-                                            map.setZoom(brStates[stateOption].zoom);
-
-                                            await sleep(1000);
-
-                                            const localCenter = LatLngToCenter(local);
-                                            setCenter(localCenter);
-                                            map.panTo(localCenter);
-
-                                            map.setZoom(8);
-                                        }
-                                    }}
-                                />
-                            </div>
-                            <div style={{ display: 'flex', padding: 8 }}>
-                                <Autocomplete
-                                    id="disease_select"
-                                    options={diseases}
-                                    autoComplete
-                                    includeInputInList
-                                    renderInput={(params) => <TextField {...params} label="Doença" />}
-                                    sx={{ width: 300 }}
-                                    value={diseases.find(
-                                        (option) => option.currentTarget?.getAttribute('data-option-index') === diseaseOption
-                                    )}
-                                    onChange={async (option) => {
-                                        const op = parseInt(option.currentTarget.getAttribute('data-option-index'), 10);
-                                        setDesease(op);
-                                        heatmap.setMap(null);
-
-                                        if (typeof heatmap === 'object') heatmap.setData([]);
-
-                                        heatmap.setMap(map);
-
-                                        for (let i = 0; i < 100; i += 1) {
-                                            fakeData?.push(new window.google.maps.LatLng(disease1[op][i].lat, disease1[op][i].lng));
-                                        }
-
-                                        heatmap.setData(fakeData);
-                                        heatmap.setOptions({ radius: 10, map, data: fakeData });
-                                        setHeatmap(heatmap);
-                                    }}
-                                />
-                            </div>
-                        </div>
-                    </GoogleMap>
+                    />
                 </div>
             </div>
         </MainCard>
